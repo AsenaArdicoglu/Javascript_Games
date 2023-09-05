2e7e7b2bb3d5a677c015a93b62b42ad65b877034
Asena Ardıçoğlu <asenaardicc@gmail.com>
2023-09-05 16:06:17 +0300

    wordList js

---
+++ hangman game/javascrit file/wordList.js
@@ -0,0 +1,86 @@
+const wordList= [
+    {
+        word: "guitar",
+        hint: "A musicial instrument with strings"
+    },
+
+    {
+        word: "oxygen",
+        hint: "A colorless, odorless gas essential for life"
+    },
+
+    {
+        word: "mountain",
+        hint: "A large natural elevation of the earth's surface"
+    },
+
+    {
+        word: "painting",
+        hint: "An art from using colors on a surface to create images or expressions"
+    },
+
+    {
+        word: "biycle",
+        hint: "A human-powered vehicle with two wheels"
+    },
+
+    {
+        word: "astronomy",
+        hint: "The systematic study of celestial objects and phenomena."
+    },
+    {
+        word: "science",
+        hint: "The systematic study of the structure  and behavior of the pyhscial and soul."
+    },
+
+    {
+        word: "sunset",
+        hint: "The daily disappearence of the Sun below the horizon"
+    },
+    {
+        word: "coffee",
+        hint: "A popular caffeinated beverages made from roasted coffee beans"
+    },
+    {
+        word: "dance",
+        hint: "A rhythmic movement of the body often performed to music"
+    },
+    {
+        word: "adventure",
+        hint: "An exciting or daring experience"
+    },
+    {
+        word: "diamond",
+        hint: "A precious gemstone known for its brilliance and hardness"
+    },
+
+    {
+        word: "camera",
+        hint: "A device used to capture and record images or videos"
+    },
+    {
+        word: "jazz",
+        hint: "A generate of music characterized by improvisation and syncopation"
+    },
+    {
+        word: "pizza",
+        hint: "A savory dish consisting of  a round, flattened base with toppings"
+    },
+    {
+        word: "history",
+        hint: "The study of past events and human civilization"
+    },
+    {
+        word: "butterfly",
+        hint: "An insect with colorful wings and a slender body"
+    },
+
+    {
+        word: "chocolate",
+        hint: "A sweet treat made from coco beans"
+    },
+    {
+        word: "football",
+        hint: "A popular sport played with a spherical ball"
+    },
+]
